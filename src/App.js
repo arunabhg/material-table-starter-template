f86032d230d01968f2f974b93b473969133cf7b2
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-09-18 20:05:39 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MaterialTable from "@material-table/core";
+import Edit from "@material-ui/icons/Edit";
+
+import "./App.css";
+
+const lookup = { true: "Available", false: "Unavailable" };
+
+const DEMO_COLUMNS = [
+	{ title: "First Name", field: "firstName" },
+	{ title: "Last Name", field: "lastName" },
+	{
+		title: "Birth Year",
+		field: "birthYear",
+		type: "numeric",
+		cellStyle: { textAlign: "left" }
+	},
+	{ title: "Availability", field: "availability", lookup }
+];
+
+const DEMO_DATA = [
+	{ firstName: "Tod", lastName: "Miles", birthYear: 1987, availability: true },
+	{ firstName: "Jess", lastName: "Smith", birthYear: 2000, availability: false }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+	return (
+		<div className="App">
+			<div className="Table">
+				<MaterialTable
+					title="Employee Data"
+					columns={DEMO_COLUMNS}
+					data={DEMO_DATA}
+					actions={[
+						{
+							icon: () => <Edit />,
+							tooltip: "Edit",
+							onClick: (event, rowData) => {
+								const rowJson = JSON.stringify(rowData, null, 2);
+								alert(`Passed value in Edit : ${rowJson}`);
+							}
+						}
+					]}
+					options={{
+						actionsColumnIndex: -1
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
